0ea70d190b99196eb0367130b02d52cc0c676479
yewcm123 <yewcm123@gmail.com>
2024-02-12 20:19:07 +0800

    Added back button to signup page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,12 +1,23 @@
-import SignUpForm, { SignUpValues } from './components/sign-up-form'
+import { Button } from '@mui/material'
+import SignUpForm from './components/sign-up-form'
+import { useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
+  const navigate = useNavigate()
   return (
     <div className="align-middle">
-      <h1 className='mb-10'>Sign Up</h1>
-      <SignUpForm onSubmit={(values: SignUpValues) => {
-        console.log(values)
-      }} />
+      <h1 className="mb-10">Sign Up</h1>
+      <SignUpForm />
+      <Button
+        variant="text"
+        fullWidth
+        sx={{ paddingY: '1em' }}
+        onClick={() => {
+          navigate('/')
+        }}
+      >
+        Back
+      </Button>
     </div>
   )
 }
