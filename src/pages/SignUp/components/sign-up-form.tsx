71bec3b0afbc1e7029dde5350c2b80b82302730c
Derrick Yew <derrick.yew@flex-solver.com.sg>
2024-02-13 12:56:36 +0800

    fix: updated sign up form to take in phone number, removed obsolete sign up form

--- src/pages/SignUp/components/sign-up-form.tsx
+++ src/pages/SignUp/components/sign-up-form.tsx
@@ -7,22 +7,22 @@ import {
   Select,
 } from '@mui/material'
 import { Formik } from 'formik'
-import { MuiTelInput } from 'mui-tel-input'
 import { Form, useNavigate } from 'react-router-dom'
 import { signUpSchema } from '../../../schemas/index'
 import { Occupation } from '../../../utils/constants'
 import InputField from './InputField'
+import MuiPhoneNumber from 'material-ui-phone-number'
 
 const SignUpForm: React.FC = () => {
- const navigate = useNavigate()
+  const navigate = useNavigate()
 
- const onSubmit = async (values, actions) => {
-  console.log(values)
-  console.log(actions)
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-  actions.resetForm()
-  navigate('/home')
-}
+  const onSubmit = async (values, actions) => {
+    console.log(values)
+    console.log(actions)
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    actions.resetForm()
+    navigate('/home')
+  }
 
   return (
     <Formik
@@ -38,120 +38,130 @@ const SignUpForm: React.FC = () => {
         confirmPassword: '',
       }}
     >
-      {(props) => (
-        <Form autoComplete='on'>
-          <div className="mb-5">
-            <div className="gap-5 mb-5 md:flex">
-              <div className="flex-grow mb-5 md:mb-0">
-                <InputField
-                  fullwidth
-                  label="First Name"
-                  name="firstName"
-                  type="text"
-                  placeholder="First Name"
-                />
-              </div>
-              <div className="flex-grow ">
-                <InputField
-                  fullwidth
-                  label="Last Name"
-                  name="lastName"
-                  type="text"
-                  placeholder="Last Name"
-                />
-              </div>
-            </div>
-            {/* <div className="mb-5">
-              <MuiTelInput
-                fullWidth
-                label="Phone Number"
-                name='phoneNumber'
-                placeholder="Phone Number"
-                value={props.values.phoneNumber}
-                onChange={props.handleChange}
-                onBlur={props.handleBlur}
-                error={
-                  props.touched.phoneNumber && Boolean(props.errors.phoneNumber)
-                }
-                helperText={
-                  props.touched.phoneNumber && props.errors.phoneNumber
-                }
-              />
-            </div> */}
+      {(props) => {
+        return (
+          <Form onSubmit={props.handleSubmit} autoComplete="on" autoSave="on">
             <div className="mb-5">
-              <FormControl
-                variant="outlined"
-                fullWidth
-                error={
-                  props.touched.occupation && Boolean(props.errors.occupation)
-                }
-              >
-                <InputLabel>Occupation</InputLabel>
-                <Select
-                  name="occupation"
-                  placeholder="occupation"
-                  label="Occupation"
+              <div className="gap-5 mb-5 md:flex">
+                <div className="flex-grow mb-5 md:mb-0">
+                  <InputField
+                    fullwidth
+                    label="First Name"
+                    name="firstName"
+                    type="text"
+                    placeholder="John"
+                  />
+                </div>
+                <div className="flex-grow ">
+                  <InputField
+                    fullwidth
+                    label="Last Name"
+                    name="lastName"
+                    type="text"
+                    placeholder="Doe"
+                  />
+                </div>
+              </div>
+              <div className="mb-5">
+                <MuiPhoneNumber
+                  onlyCountries={['my', 'sg']}
                   fullWidth
-                  onChange={props.handleChange}
+                  variant="outlined"
+                  label="Phone Number"
+                  name="phoneNumber"
+                  placeholder="Phone Number"
+                  value={props.values.phoneNumber}
+                  onChange={(e) => {
+                    props.handleChange(e)
+                    props.setFieldValue('phoneNumber', e)
+                  }}
                   onBlur={props.handleBlur}
-                  value={props.values.occupation}
+                  error={
+                    props.touched.phoneNumber &&
+                    Boolean(props.errors.phoneNumber)
+                  }
+                  helperText={
+                    props.touched.phoneNumber && props.errors.phoneNumber
+                  }
+                  defaultCountry={'my'}
+                />
+              </div>
+              <div className="mb-5">
+                <FormControl
+                  variant="outlined"
+                  fullWidth
+                  error={
+                    props.touched.occupation && Boolean(props.errors.occupation)
+                  }
                 >
-                  <MenuItem value={Occupation.FRONTEND_DEVELOPER}>
-                    Frontend Developer
-                  </MenuItem>
-                  <MenuItem value={Occupation.BACKEND_DEVELOPER}>
-                    Backend Developer
-                  </MenuItem>
-                  <MenuItem value={Occupation.FULL_STACK_DEVELOPER}>
-                    Full Stack Developer
-                  </MenuItem>
-                </Select>
-                {props.touched.occupation &&
-                  Boolean(props.errors.occupation) && (
-                    <FormHelperText>{props.errors.occupation}</FormHelperText>
-                  )}
-              </FormControl>
+                  <InputLabel>Occupation</InputLabel>
+                  <Select
+                    name="occupation"
+                    placeholder="occupation"
+                    label="Occupation"
+                    fullWidth
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                    value={props.values.occupation}
+                  >
+                    <MenuItem value={Occupation.FRONTEND_DEVELOPER}>
+                      Frontend Developer
+                    </MenuItem>
+                    <MenuItem value={Occupation.BACKEND_DEVELOPER}>
+                      Backend Developer
+                    </MenuItem>
+                    <MenuItem value={Occupation.FULL_STACK_DEVELOPER}>
+                      Full Stack Developer
+                    </MenuItem>
+                  </Select>
+                  {props.touched.occupation &&
+                    Boolean(props.errors.occupation) && (
+                      <FormHelperText>{props.errors.occupation}</FormHelperText>
+                    )}
+                </FormControl>
+              </div>
+              <div className="mb-5">
+                <InputField
+                  label="Email"
+                  name="email"
+                  type="text"
+                  placeholder="Email"
+                  fullwidth
+                />
+              </div>
+              <div className="mb-5">
+                <InputField
+                  label="Password"
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  fullwidth
+                  
+                />
+              </div>
+              <div className="mb-5">
+                <InputField
+                  label="Confirm Password"
+                  name="confirmPassword"
+                  type="password"
+                  placeholder="Confirm Password"
+                  fullwidth
+                />
+              </div>
+              <Button
+                variant="contained"
+                size="large"
+                type="submit"
+                disabled={props.isSubmitting}
+                fullWidth
+                sx={{ paddingY: '1em' }}
+              >
+                Sign Up
+              </Button>
             </div>
-            <div className="mb-5">
-              <InputField
-                label="Email"
-                name="email"
-                type="text"
-                placeholder="Email"
-                fullwidth
-              />
-            </div>
-            <div className="mb-5">
-              <InputField
-                label="Password"
-                name="password"
-                type="password"
-                placeholder="Password"
-                fullwidth
-              />
-            </div>
-            <div className="mb-5">
-              <InputField
-                label="Confirm Password"
-                name="confirmPassword"
-                type="password"
-                placeholder="Confirm Password"
-                fullwidth
-              />
-            </div>
-            <Button
-              variant="contained"
-              size="large"
-              type="submit"
-              disabled={props.isSubmitting}
-              fullWidth
-              sx={{ paddingY: '1em' }}
-            >
-              Sign Up
-            </Button>
-          </div>
-        </Form>
-      )}
+          </Form>
+        )
+      }}
     </Formik>
   )
 }
