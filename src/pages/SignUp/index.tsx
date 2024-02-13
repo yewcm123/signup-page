1d558bcb248e9621b6b1ffbda34805c07ca49731
Derrick Yew <derrick.yew@flex-solver.com.sg>
2024-02-13 10:08:57 +0800

    fix: change signup form width in desktop mode

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 const SignUp = () => {
   const navigate = useNavigate()
   return (
-    <div className="w-10/12 m-auto align-middle">
+    <div className="md:w-5/12 m-auto align-middle">
       <h1 className="mb-10">Sign Up</h1>
       <SignUpForm />
       <Button
