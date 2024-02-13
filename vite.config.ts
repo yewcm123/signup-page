4bd540bdfdf0785c664be40ab27ce5eaf47df078
Derrick Yew <derrick.yew@flex-solver.com.sg>
2024-02-13 14:36:20 +0800

    package.json and vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/repo/",
+  base: "/signup-page/",
   plugins: [react()],
   server: {
     host: true
